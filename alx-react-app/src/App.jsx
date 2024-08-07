f94bfd163bd42f4752166fd9582888b078178dec
Leon Gabriel <g73346920@gmail.com>
2024-08-07 12:09:41 +0300

    new header tag added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
 function App() {
   const [count, setCount] = useState(0)
 
