dc14b947860f9438b0d7ee124ed6a622e87af97b
Leon Gabriel <g73346920@gmail.com>
2024-08-21 12:46:39 +0300

    First commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,14 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Footer from './Components/Footer'
-import MainContent from './Components/MainContent'
-import WelcomeMessage from './Components/WelcomeMessage'
-import UserProfile from './Components/UserProfile'
+import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
-import Counter from './components/Counter'
-
-
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -35,12 +32,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Footer />
+      <WelcomeMessage />
       <Header />
       <MainContent />
-      <WelcomeMessage />
+      <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Counter />
     </>
   )
 }
--- alx-react-app-new/src/Components/Footer.jsx
+++ alx-react-app-new/src/Components/Footer.jsx
@@ -1,10 +1,10 @@
 function Footer() {
-  return(
-    <footer style={{fontFamily:'100',fontWeight:'bold',color:'brown'}}>
-      <p>&copy; 2023 City Lovers.
-      </p>
-    </footer>
-  );
-  }
-  
-  export default Footer;
+return(
+  <footer style={{fontFamily:'100',fontWeight:'bold',color:'brown'}}>
+    <p>&copy; 2023 City Lovers.
+    </p>
+  </footer>
+);
+}
+
+export default Footer;
