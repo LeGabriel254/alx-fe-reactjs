a97b6b97e48009333943bbd6d483cf58c5b0ba70
Leon Gabriel <g73346920@gmail.com>
2024-08-23 15:19:04 +0300

    update

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -9,12 +9,17 @@ import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 import ProfilePage from './ProfilePage';
 import UserContext from './components/UserContext'
+import UserDetails from './components/UserDetails'
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return <ProfilePage userData={userData} />;
-  <UserContext />
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  
+  )
 }
 
 export default App
