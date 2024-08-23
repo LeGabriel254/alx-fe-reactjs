7ec9d5623bbebb5099cfbc3ab011a370b9e1f63d
Leon Gabriel <g73346920@gmail.com>
2024-08-23 19:57:22 +0300

    new link updated

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -9,6 +9,16 @@
          <div key={recipe.id}>
            <h3>{recipe.title}</h3>
            <p>{recipe.description}</p>
+           <h1>Recipe List</h1>
+      <ul>
+          <li key={recipe.id}>
+            {/* Using Link instead of anchor tag */}
+            <Link to={`/recipes/${recipe.id}`}>
+              <h2>{recipe.title}</h2>
+            </Link>
+            <p>{recipe.description}</p>
+          </li>
+      </ul>
          </div>
        ))}
 
