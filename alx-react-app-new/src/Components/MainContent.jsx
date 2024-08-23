d014c295153e61acf8a1145f082eca1405cfcac7
Leon Gabriel <164190975+LeGabriel254@users.noreply.github.com>
2024-08-23 12:59:41 +0300

    Delete alx-react-app-new directory

--- alx-react-app-new/src/Components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-function MainContent(){
-  return(
-    <main style={{ border:'2px solid brown', marging:'12px', padding:'12px'}}>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
-  );
-}
-
-export default MainContent;
