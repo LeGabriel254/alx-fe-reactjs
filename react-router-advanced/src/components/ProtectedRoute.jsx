61ac05e03f54df49512a67b415bc46144311b4f6
Leon Gabriel <g73346920@gmail.com>
2024-08-30 19:56:25 +0300

    modified

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import {Navigate} from 'react-router-dom';
-import UseAuth from './UseAuth.js'
-
 function ProtectedRoute({children}){
   if(!isAuthenticated()){
     //Move to home if not authenticated
-    return <navigate to="/" replace />;
+    return <navigate to="useAuth" replace />;
   }
   return children;
 }
