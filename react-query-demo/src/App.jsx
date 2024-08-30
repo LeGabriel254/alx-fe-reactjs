bb0ea743b88a243c76e31666684b6fd9fa71a899
Leon Gabriel <g73346920@gmail.com>
2024-08-30 18:35:32 +0300

    modified

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,13 +1,20 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './component/PostsComponent.jsx';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import PostsComponent from './PostsComponent';
 
+const queryClient = new QueryClient();
 
-  const queryClient = new QueryClient();
-
-  function App() {
-    return (
-      <QueryClientProvider client={queryClient}>
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo</h1>
         <PostsComponent />
-      </QueryClientProvider>
-    );
-  }
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
