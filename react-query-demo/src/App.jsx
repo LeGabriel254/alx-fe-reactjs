49d6679767f1cc3c12a1da994fe45d4f3d5f2af5
Leon Gabriel <g73346920@gmail.com>
2024-08-30 18:27:38 +0300

    modified

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,13 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './components/PostsComponent.JSX';
+import PostsComponent from './component/PostsComponent.jsx';
 
-const queryClient = new QueryClient();
 
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <PostsComponent />
-    </QueryClientProvider>
-  );
-}
+  const queryClient = new QueryClient();
+
+  function App() {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
+    );
+  }
