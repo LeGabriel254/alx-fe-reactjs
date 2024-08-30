7e8a7299ca6238b563ff5b50ee36b62ab5522fe2
Leon Gabriel <g73346920@gmail.com>
2024-08-30 19:41:24 +0300

    ProtectedRoute added

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Home from './components/Home';
 import Profile from './components/Profile';
 import BlogPost from './components/BlogPost';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -11,7 +12,15 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/profile/*" element={<Profile />} />
         <Route path="/blog/:id" element={<BlogPost />} />
-   
+        {/*ProtectedRoute*/}
+        <Route 
+        path='/profile/*'
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+   />
       </Routes>
     </Router>
   );
---
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Navigate} from 'react-router-dom';
+
+function ProtectedRoute({children}){
+  if(!isAuthenticated()){
+    //Move to home if not authenticated
+    return <navigate to="/" replace />;
+  }
+  return children;
+}
+
+export default ProtectedRoute;
