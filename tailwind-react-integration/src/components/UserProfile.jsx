556d6a166fd5c1faea57d2f8e7c4394380c8fd9f
Leon Gabriel <g73346920@gmail.com>
2024-09-06 15:35:14 +0300

    Modified

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,8 +4,8 @@ import React from 'react'
   return (
    <div  className='container bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 ms:max-w-xs o md:max-w-sm '>
    <img src="https://via.placeholder.com/150 rounded-full w-36 h-36 mx-auto"  alt="User"/>
-   <h1 className='Name text-xl text-blue-800 my-4 text-lg 0n sm:screens and text-xl on md:screens and'>John Doe</h1>
-    <p className='about text-gray-600 text-base'>Developer at Example Co. Loves to write code and explore new technologies</p>
+   <h1 className='Name text-xl text-blue-800 my-4 md:text-lg sm:text-xl'>John Doe</h1>
+    <p className=' text-gray-600 text-base'>Developer at Example Co. Loves to write code and explore new technologies</p>
    </div>
   );
 }
