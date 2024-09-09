fe152f07bdf6e5ebb9e806545cf6e6ad076b128c
Leon Gabriel <g73346920@gmail.com>
2024-09-09 15:04:22 +0300

    modified

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import HomePage from './components/HomePage'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="bg-gradient-to-b from-beige-200 to-green-900 h-screen flex flex-col items-center justify-between">
    <HomePage />
-    </>
+   <Header />
+    </div>
   )
 }
 
