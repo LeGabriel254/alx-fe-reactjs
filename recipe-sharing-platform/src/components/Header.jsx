6ac7b131d91f59b8c9775ba7014f5682e3f0f491
Leon Gabriel <g73346920@gmail.com>
2024-09-09 15:02:29 +0300

    modified

---
+++ recipe-sharing-platform/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AppHeader = () => {
+  return (
+    <header className="flex flex-col items-center py-4 ">
+      <div className="bg-white rounded-lg shadow-lg p-2 text-3xl absolute inset-x-0 top-0 w-20rem...">
+        {/* <img
+          src="/path/to/your-logo.png" // Replace with your logo URL
+          alt="App Logo"
+          className="h-12 w-12"
+        /> */}
+      <h1 className="text-green-700 text-2xl mt-2 justify-self-center ...">Healthy Lifestyle Apphome</h1>
+      </div>
+    </header>
+  );
+};
+
+export default AppHeader;
