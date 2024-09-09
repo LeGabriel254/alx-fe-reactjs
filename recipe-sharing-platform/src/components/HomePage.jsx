c0507f3abdb358a31f1c9a65459ef17b73e0e578
Leon Gabriel <g73346920@gmail.com>
2024-09-09 15:03:14 +0300

    chore:useEffected rendered

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,10 +1,30 @@
 import React, {useState, useEffect} from 'react'
 
 
-const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
 
-export default HomePage
+const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    // Fetch the recipe data from the data.json file
+    fetch("https://via.placeholder.com/150")
+      .then((response) => response.json())
+      .then((data) => setRecipes(data));
+  }, [url]);
+
+  return (
+   <>return (
+    <div className="recipe-container">
+      {recipes.map((recipe) => (
+        <div className="card" key={recipe.id}>
+          <img src={recipe.image} alt={recipe.title} />
+          <h2>{recipe.title}</h2>
+          <p>{recipe.summary}</p>
+        </div>
+      ))}
+    </div>
+  );</>
+  );
+};
+
+export default HomePage;
