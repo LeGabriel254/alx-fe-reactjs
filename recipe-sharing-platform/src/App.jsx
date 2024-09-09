586992e5bee5ef04fb6343cdecccbbc976127ad7
Leon Gabriel <g73346920@gmail.com>
2024-09-09 11:10:16 +0300

    chore:import the HomePage

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import HomePage from './components/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+   <HomePage />
     </>
   )
 }
