4265075f05a1cea11a5a31c361022e63fd4fb326
Leon Gabriel <g73346920@gmail.com>
2024-09-10 14:48:09 +0300

    Modified

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,28 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import image from  "./images "
+import React, { Suspense, useState, useEffect } from 'react';
+import spaghetti from "./images/spaghetti.jpg"
 const HomePage = () => {
   const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false)
 
   // Fetch the recipes from data.json when the component mounts
   useEffect(() => {
-    fetch('/data.json')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+    async function fetchData( ) {
+      setLoading(true)
+      try {
+        const response = await fetch("/data.json"); 
+        if (response.status === "ok") {
+          // INFO: use this is used if you wan to persiste the data
+          // if (recepies.length > 0) {
+          //   // spread the existing data
+          //   setRecipes([...recepies, ...response.json()])
+          // } 
+            setRecipes([...response.json()])
         }
-        return response.json();
-      })
-      .then((data) => setRecipes(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
+
+      } catch (error) {
+        // alert("Something happened")
+        // only in dev mode
+        console.log(error)
+        throw new Error(error)
+      } finally {
+        setLoading(false)
+      }
+    } 
+    fetchData()
+    }, [
+      // trigger to watch when something changes to reload the recepies.
+    ]);
 
   return (
     <div className="container mx-auto py-10 md:p-8 ">
       <h1 className="text-4xl font-bold text-center mb-8">Recipe Collection</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+       <Suspense fallBack={<Loading />}>
         {recipes.map((recipe) => (
           <div key={recipe.id} className="bg-white shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-300">
-            <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
+            <img src={spaghetti} alt={recipe.title} className="w-full h-48 object-cover" />
             <div className="p-6">
               <h2 className="text-2xl font-bold mb-2">{recipe.title}</h2>
               <p className="text-gray-700 mb-4">{recipe.summary}</p>
@@ -32,9 +50,15 @@ const HomePage = () => {
             </div>
           </div>
         ))}
+       </Suspense>
       </div>
     </div>
   );
 };
+
+
+const Loading = () => (
+  <div>Loaidng....</div>
+)
 
 export default HomePage;
