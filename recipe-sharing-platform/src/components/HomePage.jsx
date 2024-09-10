a0d01df192d3103470dc2cb2e2a74c7905e929c2
Leon Gabriel <g73346920@gmail.com>
2024-09-10 13:21:08 +0300

    Modified

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto py-10">
+    <div className="container mx-auto py-10 md:p-8 ">
       <h1 className="text-4xl font-bold text-center mb-8">Recipe Collection</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {recipes.map((recipe) => (
