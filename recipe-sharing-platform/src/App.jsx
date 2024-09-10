4fe77879e5ab855751cd7328fdc9e0687cd78468
Leon Gabriel <g73346920@gmail.com>
2024-09-10 22:53:33 +0300

    routes render

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import HomePage from './components/HomePage'
 import Header from './components/Header'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-gradient-to-b from-beige-200 to-green-900 h-screen flex flex-col items-center justify-between">
-   <HomePage />
-   <Header />
-    </div>
+   
+      <Router className="bg-gradient-to-b from-beige-200 to-green-900 h-screen flex flex-col items-center justify-between">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   )
 }
 
