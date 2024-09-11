229adffa1379ed849d468042c2dc2c6620627fdf
Leon Gabriel <g73346920@gmail.com>
2024-09-11 14:32:30 +0300

    Fetch recipes from data.json

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -9,38 +9,26 @@ const HomePage = () => {
     async function fetchData( ) {
       setLoading(true)
       try {
-        const response = await fetch("/data.json"); 
-        if (response.status === "ok") {
-          // INFO: use this is used if you wan to persiste the data
-          // if (recepies.length > 0) {
-          //   // spread the existing data
-          //   setRecipes([...recepies, ...response.json()])
-          // } 
-            setRecipes([...response.json()])
-        }
-
+        const response = await fetch("/public/data.json"); 
+        const data = await response.json()          
+        setRecipes([...data])
       } catch (error) {
-        // alert("Something happened")
-        // only in dev mode
-        console.log(error)
         throw new Error(error)
       } finally {
         setLoading(false)
       }
     } 
     fetchData()
-    }, [
-      // trigger to watch when something changes to reload the recepies.
-    ]);
-
-  return (
+  }, [])
+    
+    return (
     <div className="container mx-auto py-10 md:p-8 ">
       <h1 className="text-4xl font-bold text-center mb-8">Recipe Collection</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-       <Suspense fallBack={<Loading />}>
+       <Suspense fallback={<Loading />}>
         {recipes.map((recipe) => (
           <div key={recipe.id} className="bg-white shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-300">
-            <img src={spaghetti} alt={recipe.title} className="w-full h-48 object-cover" />
+            <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
             <div className="p-6">
               <h2 className="text-2xl font-bold mb-2">{recipe.title}</h2>
               <p className="text-gray-700 mb-4">{recipe.summary}</p>
