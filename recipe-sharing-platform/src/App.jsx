37e05f37855a979bf2744f11f73951cd6f90a2ab
Leon Gabriel <g73346920@gmail.com>
2024-09-11 14:33:24 +0300

    Imported files

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 import HomePage from './components/HomePage'
 import Header from './components/Header'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeDetail from "./components/RecipeDetail"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   
-      <Router className="bg-gradient-to-b from-beige-200 to-green-900 h-screen flex flex-col items-center justify-between">
+    <Router className="bg-gradient-to-b from-beige-200 to-green-900 h-screen flex flex-col items-center justify-between">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
@@ -18,4 +19,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
