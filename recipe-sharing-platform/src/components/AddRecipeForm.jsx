ee5901a41bee33f46e521e03c7e58bf2b5c66b06
Leon Gabriel <g73346920@gmail.com>
2024-09-13 15:54:16 +0300

    chored:Textarea modified

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -1,11 +1,11 @@
 import React, {useState} from 'react'
 
 const AddRecipeForm = () => {
-const [title, setTittle]= useState("");
+const [title, setTitle]= useState("");
 const [ingredients, setIngredient]= useState("");
 const [steps, setSteps] = useState("");
 
-// Handling form submssion
+// Handling form submision
 const handleSubmit = () =>
 e.preventDefault();
 
@@ -18,6 +18,27 @@ console.log('Recipe Submitted:' , recipeDetails) /* one can send recipeDetails t
   return (
     <div>
  <h1>Submit Your Recipe </h1>
+
+ <form onSubmit={handleSubmit}>
+ {/* /*Recipe title  */}
+  <div>
+    <label htmlFor="title">Recipe title</label> <br />
+    <input type="text" id='title' value= {title} onChange={(e) => setTitle(e.target.value)} required/> 
+  </div><br />
+   {/* //Ingredient */}
+   <div>
+    <label htmlFor="ingredients">Ingredien</label>
+   <textarea id="Ingredients" cols="50" rows="6" value={ingredients}  onChange={(e)=> setIngredient(e.target.value)} placeholder='List ingredient here..' Required/>
+   </div><br />
+
+   {/* //Preparation steps */}
+   <div>
+    <label htmlFor="steps">Preparation Steps</label> <br />
+    <textarea  id="steps" cols="50" rows="8" value={steps}  onChange={(e) => setSteps(e.target.value)} placeholder='List the steps here ...' Required />
+   </div>
+   {/* sumitButton */}
+   <button type="submit">Submit Recipe</button>
+ </form>
     </div>
   )
 }
