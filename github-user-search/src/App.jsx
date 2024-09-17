01ec73c6ce9c2eabeea551def3b020700d4a73c3
Leon Gabriel <g73346920@gmail.com>
2024-09-17 16:14:49 +0300

    react-router-dom installed

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css'
+import './services'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router>
+      <Route>
+
+      </Route>
+    </Router>
     
     </>
   )
