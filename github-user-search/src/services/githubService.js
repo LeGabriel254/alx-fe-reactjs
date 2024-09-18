b784ae48058a4ff146ba265f0363e2fad853ad70
Leon Gabriel <g73346920@gmail.com>
2024-09-18 22:56:28 +0300

    Fetch user data from GitHub

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react'
+import axios from 'axios'
+// Function to fetch user data from GitHub API
+export const fetsUserData = async (userName) => {
+  useEffect(() =>{
+    axios.get('https://api.github.com/users/{username}') /*Make an API call using axios to get user data from Github*/
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+  },[])
+}
