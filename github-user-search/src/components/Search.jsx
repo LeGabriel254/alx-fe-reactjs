0c7327b1683818b74e666fa97c0d476859fcb179
Leon Gabriel <g73346920@gmail.com>
2024-09-19 23:39:39 +0300

    Modified

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -33,43 +33,44 @@ const search = () => {
     <div>
       <form onSubmit={handleSubmit} className='btn gap-3'>
         <div>
-          <input  className='in'
-          type="text" value={username} 
-          onChange={(e) => setUsename(e.target.value)} 
-          placeholder='Enter GitHub username' required 
+          <input className='in'
+            type="text" value={username}
+            onChange={(e) => setUsename(e.target.value)}
+            placeholder='Enter GitHub username' required
           />
         </div>
         <div className='btn gap-4'>
           <h3 className='login text-neutral-900 underline font-serif font-weight: 300;'>Login</h3>
           <label>Email:</label>
-          <input 
-          type="email" 
-          value={email} 
-           onchange={(e) => 
-            setEmail(e.target.value)}
-           />
+          <input
+            type="email"
+            value={email}
+            onchange={(e) =>
+              setEmail(e.target.value)}
+          />
         </div>
         <div>
-            <label>Password:</label>
-            <input type="password"
+          <label>Password:</label>
+          <input type="password"
             value={password}
-            onchange={(e) => setPassword(e.target.value)} />
+            onchange={(e) =>
+              setPassword(e.target.value)} />
         </div>
         <button type='submit'>Search</button>
-        </form>
-        {loading && <p>Loading...</p>}
-        {error && <p> {error}</p>}
-        {userData && (
-          <div>
-            <img src={userData.avatar_url} alt={userData.name} width={100} />
-            <h3>{userData.name}</h3>
-            <p>{userData.bio}</p>
-            <a href={userData.html_url} target='_blank' rel="noopener noreferrer">View Profile</a>
-          </div>
-        )}
-     
+      </form>
+      {loading && <p>Loading...</p>}
+      {error && <p> {error}</p>}
+      {userData && (
+        <div>
+          <img src={userData.avatar_url} alt={userData.name} width={100} />
+          <h3>{userData.name}</h3>
+          <p>{userData.bio}</p>
+          <a href={userData.html_url} target='_blank' rel="noopener noreferrer">View Profile</a>
+        </div>
+      )}
+
     </div>
   )
-}
+};
 
-export default search
+export default search;
