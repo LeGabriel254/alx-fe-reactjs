2af6a6d396d8bb3d82ef0c413668f04bf3ddf3cb
Leon Gabriel <g73346920@gmail.com>
2024-09-19 22:33:11 +0300

    Modified

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,15 @@
 import React, {useEffect} from 'react'
 import axios from 'axios'
 
-// Function to fetch user data from GitHub API
- export const fetchUserData = async (username) => {
-  useEffect(() =>{
-    axios.get('https://api.github.com/users/{username}') /*Make an API call using axios to get user data from Github*/
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+export const fetchUserData = async (username) => {
+  const Api_Url = `https://api.github.com/users/{username}`
+  
+  try{
 
-  },[])
+    const response = await axios.get('https://api.github.com/users/{username}') /*Api call using axios to get user data from GitHub*/
+   //if Api call is successfull return response
+    return response.data;
+  }catch(error){
+   throw new error("user not found")
+  }
 };
-
