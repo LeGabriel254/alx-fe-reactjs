fa88a61a2db1eb86266e49b77266ad2021e05d95
Leon Gabriel <g73346920@gmail.com>
2024-09-19 22:32:19 +0300

    chored:call to get the userData

---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { fetchUserData } from '../services/githubService'
+
+const search = () => {
+  const [username, setUsename] = useState('')
+  const [userData, setUserData] = useState(null)
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    setUserData(null)
+    setLoading(true)
+
+    try {
+      const data = await fetchUserData(username);
+      setUserData(data)
+    } catch (error) {
+      setError("Looks like we can't find the user")
+    } finally {
+      setLoading(false)
+    }
+
+
+  };
+
+
+  return (
+    <div>
+      <form onChange={handleSubmit}>
+
+      </form>
+    </div>
+  )
+}
+
+export default search
