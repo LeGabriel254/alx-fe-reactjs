d135975562fa75b1b3187d9e60937e976024efa8
Leon Gabriel <g73346920@gmail.com>
2024-09-20 23:09:50 +0300

    Chored:queryString added

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import axios from 'axios'
 
-export const fetchUserData = async (username) => {
-  const Api_Url = `https://api.github.com/users/{username}`
-  
+const Api_Url = `https://api.github.com/users/{username}`
+export const fetchUserData = async (username, location,repoCount) => {
+  let query=[];
+   if(username) query.push(`q=${username}`);
+   if(location)query.push(`location:${location}`);
+   if(repoCount)query.push(`repo:>=${repoCount}`)
+
+   const queryString = query.join('+')
   try{
 
     const response = await axios.get('https://api.github.com/users/{username}') /*Api call using axios to get user data from GitHub*/
