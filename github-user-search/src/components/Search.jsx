725377d26da68e77e2b06687bc70581f63253fbe
Leon Gabriel <g73346920@gmail.com>
2024-09-20 23:48:36 +0300

    chored:State update to handle loadings

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useTransition } from 'react'
 import { fetchUserData } from '../services/githubService'
 
-const search = ({onSearch,users}) => {
+const search = ({ onSearch, users }) => {
   const [username, setUsename] = useState('')
   const [userData, setUserData] = useState(null)
   const [error, setError] = useState('')
   const [loading, setLoading] = useState(false)
- const  [location, setLocation] = useState()
- const [repoCount, setRepoCount] = useState()
+  const [location, setLocation] = useState()
+  const [repoCount, setRepoCount] = useState()
 
-  const handleSearch= async (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault();
     setError('');
     setUserData(null)
@@ -41,33 +41,33 @@ const search = ({onSearch,users}) => {
         </div>
         <div className='mb-4'>
           <label className='w-full p-2 block text-sm'>Location:</label>
-          <input  className='border rounded '
-          type="location" value={location}
-          placeholder='Enter location' 
-          onChange={(e) => setLocation(e.target.value)} />
+          <input className='border rounded '
+            type="location" value={location}
+            placeholder='Enter location'
+            onChange={(e) => setLocation(e.target.value)} />
         </div>
         <div className='mb-4'>
           <label className=''>Minimum Repositories</label>
           <input
-          type="number"
-          className="w-full p-2 border rounded"
-          value={repoCount}
-          onChange={(e) => setRepoCount(e.target.value)}
-          placeholder="Enter minimum repo count"
-        />
+            type="number"
+            className="w-full p-2 border rounded"
+            value={repoCount}
+            onChange={(e) => setRepoCount(e.target.value)}
+            placeholder="Enter minimum repo count"
+          />
         </div>
         <button type='submit' onClick={handleSearch}>Search</button>
-      {loading && <p>Loading...</p>}
-      {error && <p> {error}</p>}
-      {userData && (
-        <div>
-          <img src={userData.avatar_url} alt={userData.login} width={100} />
-          <h3>{userData.login}</h3>
-          <p>{userData.bio}</p>
-          <a href={userData.html_url} target='_blank' rel="noopener noreferrer">View Profile</a>
-        </div>
-      )}
- </form>
+        {loading && <p>Loading...</p>}
+        {error && <p> {error}</p>}
+        {userData && (
+          <div>
+            <img src={userData.avatar_url} alt={userData.login} width={100} />
+            <h3>{userData.login}</h3>
+            <p>{userData.bio}</p>
+            <a href={userData.html_url} target='_blank' rel="noopener noreferrer">View Profile</a>
+          </div>
+        )}
+      </form>
     </div>
   )
 };
