c60c3017b0e867fcdb5f48f91089aafb3ab4f88c
Leon Gabriel <g73346920@gmail.com>
2024-09-22 16:47:24 +0300

    Modified

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,13 +2,20 @@ import React from 'react'
 import axios from 'axios'
 
 const Api_Url = `https://api.github.com/users/{username}`
-export const fetchUserData = async (username, location, repoCount) => {
-  let query = [];
-  if (username) query.push(`q=${username}`);
-  if (location) query.push(`location:${location}`);
-  if (repoCount) query.push(`repo:>=${repoCount}`)
+export const fetchUserData = async ({username,location,repoCount}) => {
 
-  const queryString = query.join('+')
+
+  searchUsers: async ({ username, location, repoCount }, page = 1) => {
+    let query = [];
+    if (username) query.push(`q=${username}`);
+    if (location) query.push(`location:${location}`);
+    if (repoCount) query.push(`repos:>=${repoCount}`);
+
+    const queryString = query.join('+');
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q={query}`
+    );
   try {
 
     const response = await axios.get('https://api.github.com/users/{username}') /*Api call using axios to get user data from GitHub*/
@@ -17,4 +24,7 @@ export const fetchUserData = async (username, location, repoCount) => {
   } catch (error) {
     throw new error("user not found")
   }
-};
+
+ };
+  
+}
