c6392e62a289cafdb7ca6fd24b4e5e60da14c7d6
Leon Gabriel <g73346920@gmail.com>
2024-10-25 22:32:39 +0300

    Chored:Fetch the recipes from data.json when the component mounts.

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -8,39 +8,39 @@ const HomePage = () => {
 
   // Fetch the recipes from data.json when the component mounts
   useEffect(() => {
-    async function fetchData( ) {
+    async function fetchData() {
       setLoading(true)
       try {
-        const response = await fetch("/public/data.json"); 
-        const data = await response.json()          
+        const response = await fetch("/public/data.json");
+        const data = await response.json()
         setRecipes([...data])
       } catch (error) {
         throw new Error(error)
       } finally {
         setLoading(false)
       }
-    } 
+    }
     fetchData()
   }, [])
-    
-    return (
+
+  return (
     <div className="container mx-auto py-10 md:p-8 ">
       <h1 className="text-4xl font-bold text-center mb-8">Recipe Collection</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-       <Suspense fallback={<Loading />}>
-        {recipes.map((recipe) => (
-          <div key={recipe.id} className="bg-white shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-300">
-            <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
-            <div className="p-6">
-              <h2 className="text-2xl font-bold mb-2">{recipe.title}</h2>
-              <p className="text-gray-700 mb-4">{recipe.summary}</p>
-              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-all font-extralight">
-                View Recipe
-              </button>
+        <Suspense fallback={<Loading />}>
+          {recipes.map((recipe) => (
+            <div key={recipe.id} className="bg-white shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-300">
+              <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
+              <div className="p-6">
+                <h2 className="text-2xl font-bold mb-2">{recipe.title}</h2>
+                <p className="text-gray-700 mb-4">{recipe.summary}</p>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-all font-extralight">
+                  View Recipe
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
-       </Suspense>
+          ))}
+        </Suspense>
       </div>
     </div>
   );
