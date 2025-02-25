6a333e2d736c1ae75b56be6dbbea08f7388550dd
Leon Gabriel <g73346920@gmail.com>
2025-02-25 13:02:47 +0300

    Modifed

--- recipe-sharing-app/src/components/RecommendationsList.jsx
+++ recipe-sharing-app/src/components/RecommendationsList.jsx
@@ -1,4 +1,4 @@
-import { useRecipeStore } from '..component/recipeStore';
+import { useRecipeStore } from './recipeStore';
 
 const RecommendationsList = () => {
   const recommendations = useRecipeStore(state => state.recommendations);
