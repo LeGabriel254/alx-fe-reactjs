48f95eedb96833fd7de8d0331ad01c20b2f2561f
Leon Gabriel <g73346920@gmail.com>
2025-02-26 21:26:23 +0300

    Chored:Initial commits

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import useGitHubStore from '../store/useGitHubStore';
 
 const Search = () => {
@@ -25,14 +25,14 @@ const Search = () => {
       {loading && <p className="mt-4 text-gray-700">Loading...</p>}
       {error && <p className="mt-4 text-red-500">Looks like we can't find the user</p>}
       {userData && (
-        <div className="mt-6 p-4 bg-white shadow-lg rounded-lg flex flex-col items-center">
+        <div className="mt-6 p-4 bg-white shadow-lg rounded-lg flex flex-col items-center text-start">
           <img src={userData.avatar_url} alt={userData.login} className="w-24 h-24 rounded-full" />
           <h2 className="mt-2 text-lg font-bold">{userData.name || userData.login}</h2>
           <p className="mt-2 text-gray-600">Location: {userData.location || 'Not Available'}</p>
-          <p className="mt-1 text-gray-600">Recent Repo: {userData.recentRepo}</p>
-          <p className="mt-1 text-gray-600">Last Active: {userData.lastUpdated ? new Date(userData.lastUpdated).toLocaleDateString() : 'N/A'}</p>
           <p className="mt-1 text-gray-600">Total Repositories: {userData.totalRepos}</p>
+          <p className="mt-1 text-gray-600">Last Commited Repo: {userData.recentRepo}</p>
           <p className="mt-1 text-gray-600">Top Languages: {userData.topLanguages || 'N/A'}</p>
+          <p className="mt-1 text-gray-600">Last Seen: {userData.lastUpdated ? new Date(userData.lastUpdated).toLocaleDateString() : 'N/A'}</p>
           <a href={userData.html_url} target="_blank" rel="noopener noreferrer" className="text-blue-500 mt-2 hover:underline">Visit Profile</a>
         </div>
       )}
